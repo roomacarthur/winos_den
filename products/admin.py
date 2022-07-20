7cd823b9a88a76ed9a3832856c0c8db5dbf28e2b
roomacarthur <roomacarthur@gmail.com>
2022-07-20 15:10:00 +0100

    basic admin setup for category and products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'display_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
