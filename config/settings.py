28a067d281e98aa2ae63823c84e0b381aebd9619
roomacarthur <roomacarthur@gmail.com>
2022-07-20 16:40:48 +0100

    setup products urls readyq for dev

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     # Project related apps
+    'home',
     'profiles',
     'products',
 ]
@@ -94,7 +95,10 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
+    path('products/', include('products.urls')),
+    # path('profile/', include('profiles.urls')),
 ]
