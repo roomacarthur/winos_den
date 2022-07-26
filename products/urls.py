dca9b4d1459141cc03aa5922e53e8c4740d12ddb
roomacarthur <roomacarthur@gmail.com>
2022-07-27 00:31:46 +0100

    updated urls to allow for media

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.AllProducts.as_view(), name='products'),
-    path('<id>', views.ProductDetails.as_view(), name='product_detail'),
+    path('<int:id>', views.ProductDetails.as_view(), name='product_detail'),
     path('under-10/', views.UnderTen.as_view(), name='under-10'),
 ]
