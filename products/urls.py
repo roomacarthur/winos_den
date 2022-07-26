a22be47bc4c5af3589ebc189c40cd0aadd7567ff
roomacarthur <roomacarthur@gmail.com>
2022-07-26 19:39:30 +0100

    added updated views to urls.py

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.AllProducts.as_view(), name='home'), 
+    path('', views.AllProducts.as_view(), name='products'),
+    path('<id>', views.ProductDetails.as_view(), name='product_detail'),
+    path('under-10/', views.UnderTen.as_view(), name='under-10'),
 ]
