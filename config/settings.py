580df5d433128c8778216aee202123e14b25d2a6
roomacarthur <roomacarthur@gmail.com>
2022-07-27 18:30:02 +0100

    aws s3 url fix

--- config/settings.py
+++ config/settings.py
@@ -201,8 +201,8 @@ if 'USE_AWS' in env:
     MEDIAFILES_LOCATION = 'media'
 
     # override static and meia urls in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
