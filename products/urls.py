ae8c9f17a90f4b25b353cb239ade858b0d3fda8e
roomacarthur <roomacarthur@gmail.com>
2022-07-27 15:11:34 +0100

    added new product form url

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.AllProducts.as_view(), name='products'),
     path('<int:id>', views.ProductDetails.as_view(), name='product_detail'),
     path('under-10/', views.UnderTen.as_view(), name='under-10'),
+    path('add_product', views.AddNewProduct.as_view(), name='add_product'),
 ]
