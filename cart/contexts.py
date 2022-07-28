592ddc033346061ee3d3cbde784fadb840259a86
roomacarthur <roomacarthur@gmail.com>
2022-07-28 20:25:51 +0100

    added cart\_contents contexts to allow for access to cart items

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,9 +1,24 @@
 
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+
 def cart_contents(request):
 
     cart_items = []
     total  = 0
     item_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        item_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     context = {
         'cart_items': cart_items,
