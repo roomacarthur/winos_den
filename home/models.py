0711d195108d7b8b97fec38eef6e5ed8e3917bae
roomacarthur <roomacarthur@gmail.com>
2022-07-31 00:56:41 +0100

    added plural names for both models

--- home/models.py
+++ home/models.py
@@ -6,8 +6,14 @@ class ContactUs(models.Model):
     email = models.EmailField(max_length=80, blank=False, null=True)
     submitted_on = models.DateTimeField(auto_now_add=True)
     message = models.TextField(blank=False, null=True)
+    
+    class Meta:
+        verbose_name_plural  =  "Contact Us"
 
 
 class NewsLetter(models.Model):
     email = models.EmailField(max_length=80, blank=False, null=True)
     signed_up_on = models.DateTimeField(auto_now_add=True)
+        
+    class Meta:
+        verbose_name_plural  =  "News Letter"
