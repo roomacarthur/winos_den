7759dc547309f22c5f87995a093b04b3034f9be6
roomacarthur <roomacarthur@gmail.com>
2022-07-30 13:15:24 +0100

    contact and news letter views!

--- home/views.py
+++ home/views.py
@@ -1,7 +1,42 @@
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from .forms import NewsLetterForm, ContactForm
+from .models import ContactUs, NewsLetter
+from django.urls import reverse
+from django.contrib import messages
 
 class IndexView(TemplateView):
     """
     A view to return the index page, rendered on the index.html template.
     """
     template_name = 'home/index.html'
+
+# def contact_us():
+#     #as
+
+class ContactUs(CreateView):
+
+    model = ContactUs
+    form_class = ContactForm
+    template_name = 'home/contact_us.html'
+
+    def get_success_url(self):
+        """
+        Upon successful submission return user to 
+        Prompt user with success message that includes product name.
+        """
+        messages.success(self.request, 'Thank you for contacting us! We aim to reply within 24 hours.')
+        return reverse('contact_us')
+
+class NewsLetter(CreateView):
+
+    model = NewsLetter
+    form_class = NewsLetterForm
+
+    def get_success_url(self):
+        """
+        Upon successful submission return user to 
+        Prompt user with success message that includes product name.
+        """
+        messages.success(self.request, 'Thanks for Signing Up!')
+        return reverse('contact_us')
