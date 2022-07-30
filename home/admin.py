926b3d030a59dccb7353010562fea3654bcd120e
roomacarthur <roomacarthur@gmail.com>
2022-07-30 13:14:03 +0100

    setup home admin to display conatact submits and newsletter signups.

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
+from .models import ContactUs, NewsLetter
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'submitted_on',
+        'name',
+        'email',
+        'message',
+    )
+    list_filter = (
+        'name',
+        'email',
+    )
+    search_fields = (
+        'submitted_on',
+        'name',
+        'email',
+    )
+
+    ordering = ('submitted_on',)
+
+class NewsLetterAdmin(admin.ModelAdmin):
+    list_display = (
+        'email',
+        'signed_up_on',
+    )
+    list_filter = (
+        'email',
+        'signed_up_on',
+    )
+    search_fields = (
+        'email',
+        'signed_up_on',
+    )
+
+admin.site.register(ContactUs, ContactAdmin)
+admin.site.register(NewsLetter, NewsLetterAdmin)
