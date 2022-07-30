06aeb6006556239c038a0f456c40f9a6787de570
roomacarthur <roomacarthur@gmail.com>
2022-07-30 13:15:49 +0100

    generic setup for contact and newsletter forms

---
+++ home/forms.py
@@ -0,0 +1,14 @@
+from .models import ContactUs, NewsLetter
+from django import forms
+from django.forms import ModelForm
+
+class ContactForm(ModelForm):
+    class Meta:
+        model = ContactUs
+        fields = '__all__'
+
+class NewsLetterForm(ModelForm):
+    class Meta:
+        model = NewsLetter
+        fields = '__all__'
+
