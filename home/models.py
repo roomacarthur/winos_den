abde33ac2485bddd3c18c2086fc11e4723088964
roomacarthur <roomacarthur@gmail.com>
2022-07-30 13:14:39 +0100

    updated models for contact and newsletter, added date field to newsletter

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class ContactUs(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=True)
+    email = models.EmailField(max_length=80, blank=False, null=True)
+    submitted_on = models.DateTimeField(auto_now_add=True)
+    message = models.TextField(blank=False, null=True)
+
+
+class NewsLetter(models.Model):
+    email = models.EmailField(max_length=80, blank=False, null=True)
+    signed_up_on = models.DateTimeField(auto_now_add=True)
