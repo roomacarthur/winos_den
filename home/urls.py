62d26c177f84f8ef01b4701a83310eabcc0dd59d
roomacarthur <roomacarthur@gmail.com>
2022-07-31 23:55:39 +0100

    added subscription url

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
     path('contact_us/', views.ContactUs.as_view(), name='contact_us'),
+    path('subscribe/', views.NewsLetter.as_view(), name='subscribe'),
 ]
