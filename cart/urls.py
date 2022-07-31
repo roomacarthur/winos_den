d4243381841891c93a1355714a39a17cfef66bd5
roomacarthur <roomacarthur@gmail.com>
2022-07-31 03:48:50 +0100

    cart urls for adding, updating and deleting

--- cart/urls.py
+++ cart/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CartView.as_view(), name='cart'),
+    path('', views.cart_view, name='cart'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('adjust/<item_id>/', views.update_cart, name='adjust_cart'),
+    path('remove/<item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
