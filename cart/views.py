9da747a8c8f5ba5d68a1567611246d2e933297de
roomacarthur <roomacarthur@gmail.com>
2022-07-31 03:48:34 +0100

    views, for adding, updating and deleting items in cart

--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,15 @@
+from products.models import Product
 from django.views.generic import TemplateView
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse,
+    get_object_or_404
+)
 
-class CartView(TemplateView):
-    """
-    A view to return the index page, rendered on the index.html template.
-    """
-    template_name = 'cart/cart.html'
+
+def cart_view(request):
+    """ A view that renders the bag contents page """
+    return render(request, 'cart/cart.html')
 
 def add_to_cart(request, item_id):
 
@@ -20,3 +24,30 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+def update_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+
+    cart[item_id] = quantity
+    messages.success(
+        request,(f'Updated {product.name} '
+        f'quantity to {cart[item_id]}')
+    )
+
+    request.session['cart'] = cart
+    return redirect(reverse('cart'))
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping cart"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    cart = request.session.get('cart', {})
+    cart.pop(item_id)
+    messages.success(request, f'Removed {product.name} from your bag')
+    request.session['cart'] = cart
+    return HttpResponse(status=200)
