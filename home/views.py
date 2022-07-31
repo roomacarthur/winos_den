687dcabb0d6e4e6be16e29b1093f24dcbe758577
roomacarthur <roomacarthur@gmail.com>
2022-07-31 23:58:29 +0100

    tidy, and refactor code, remove redundant code.

--- home/views.py
+++ home/views.py
@@ -11,31 +11,38 @@ class IndexView(TemplateView):
     """
     template_name = 'home/index.html'
 
-# def contact_us():
-#     #as
 
 class ContactUs(CreateView):
-
+    """
+    CBV to render contact us form on contact_us.html template, 
+    upon successful submission prompt user with success message.
+    """
     model = ContactUs
     form_class = ContactForm
     template_name = 'home/contact_us.html'
     def get_success_url(self):
         """
-        Upon successful submission return user to 
-        Prompt user with success message that includes product name.
+        Upon successful submission return to contact remplate
+        Prompt user with success message.
         """
         messages.success(self.request, 'Thank you for contacting us! We aim to reply within 24 hours.')
         return reverse('contact_us')
 
+
 class NewsLetter(CreateView):
+    """
+    CBV to render newsletter signup form.
+    Upon successful signup, prompt user with success message. 
+    """
 
     model = NewsLetter
     form_class = NewsLetterForm
+    template_name = 'home/subscribe.html'
 
     def get_success_url(self):
         """
-        Upon successful submission return user to 
-        Prompt user with success message that includes product name.
+        Upon successful submission return user to homepage.
+        Prompt user with success message.
         """
         messages.success(self.request, 'Thanks for Signing Up!')
-        return reverse('contact_us')
+        return reverse('home')
