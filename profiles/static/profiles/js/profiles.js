6bc230d92c284d416d299a02197be1bb8004f214
roomacarthur <roomacarthur@gmail.com>
2022-08-01 03:52:49 +0100

    added country js

--- profiles/static/profiles/js/profiles.js
+++ profiles/static/profiles/js/profiles.js
@@ -1,1 +1,13 @@
-console.log('profile JS is loading correctly.')
+
+var countrySelected = $('#id_user_country').val();
+if(!countrySelected) {
+    $('#id_user_country').css('color', '#aab7c4');
+};
+$('#id_user_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#aab7c4');
+    } else {
+        $(this).css('color', '#046865ff');
+    }
+});
