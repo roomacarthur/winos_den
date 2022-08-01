25255969fe87721f87909e7c711cb91be244ac2b
roomacarthur <roomacarthur@gmail.com>
2022-08-01 01:38:16 +0100

    added stripe pid to order model

--- checkout/models.py
+++ checkout/models.py
@@ -28,6 +28,7 @@ class Order(models.Model):
         null=False, default=0
         )
     original_cart = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _order_no_generator(self):
         """
