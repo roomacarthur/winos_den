aeaa5deb4180e853d08bd5814791eb6575bccfb4
roomacarthur <roomacarthur@gmail.com>
2022-08-01 02:01:24 +0100

    made stock profiles view to render temp

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def profile(request):
+    """
+    display customer profile.
+    """
+    template = "profiles/profile.html"
+    context = {}
+    
+    return render(request, template, context)
