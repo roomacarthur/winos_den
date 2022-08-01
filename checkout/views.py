31aa345f9de4b41b851babff38b02bab6f1f029a
roomacarthur <roomacarthur@gmail.com>
2022-08-01 01:38:55 +0100

    added cache checkout data function

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.shortcuts import get_object_or_404, render, redirect
+from django.views.decorators.http import require_POST
+from django.shortcuts import get_object_or_404, render, redirect, HttpResponse
 from django.urls import reverse
 from django.contrib import messages
 
@@ -9,8 +10,29 @@ from .models import Order, OrderLineItem
 from .forms import OrderForm
 from products.models import Product
 
+import json
 import stripe
-# Create your views here.
+
+
+@require_POST
+def cache_checkout_data(request):
+    """
+    Cache data to allow for user to save info for later use.
+    """
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, "Unfortunately, your payment can't be \
+            processed right now. Please try again soon.")
+        return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
 
@@ -36,6 +58,9 @@ def checkout(request):
 
         if order_form.is_valid():
             order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_cart = json.dumps(cart)
             order.save()
             for item_id, item_data in cart.items():
                 try:
