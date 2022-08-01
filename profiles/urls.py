7de7ebbf5472a15f7723266e4edbc0323ddaf35c
roomacarthur <roomacarthur@gmail.com>
2022-08-01 02:10:41 +0100

    fixed url root.

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('profile/', views.profile, name="profile" ),
+    path('', views.profile, name="profile" ),
 
 ]
