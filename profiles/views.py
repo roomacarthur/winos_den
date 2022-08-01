dd2c9bc3d5326947b27bb3d34d17705f78f7b244
roomacarthur <roomacarthur@gmail.com>
2022-08-01 03:53:32 +0100

    addded request methods to views to fetch customer profile data

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-
+from django.contrib import messages
 from .models import CustomerProfile
+from .forms import CustomerProfileForm
+from checkout.models import Order
 
 # Create your views here.
 def profile(request):
@@ -8,9 +10,35 @@ def profile(request):
     display customer profile.
     """
     profile = get_object_or_404(CustomerProfile, user=request.user)
+
+    if request.method == "POST":
+        form = CustomerProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your Profile was successfully updated!")
+    form = CustomerProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = "profiles/profile.html"
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
     
+    return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a historic confirmation, for {order_number}, \
+          The Original confirmation was sent via email.'
+    ))
+
+    template = "checkout/checkout_success.html"
+    context = {
+        'order': order,
+        'form_profile': True,
+    }
+
     return render(request, template, context)
