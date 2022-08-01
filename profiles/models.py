8d2a212afb07040d5e10a57fdf5229bb56646997
roomacarthur <roomacarthur@gmail.com>
2022-08-01 02:00:59 +0100

    updated customerprofile model and added update/create method

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField 
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -8,19 +10,29 @@ class CustomerProfile(models.Model):
     """
     Customer User Profile. 
 
-    To Store Default Address
+    To Store Default info
 
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30, blank=False, null=True)
     last_name = models.CharField(max_length=50, blank=False, null=True)
-    house_name = models.CharField(max_length=50, blank=False, null=True)
-    street_address_1 = models.CharField(max_length=80, blank=False, null=True)
-    street_address_2 = models.CharField(max_length=80, blank=True, null=True)
-    city = models.CharField(max_length=80, blank=False)
-    post_code = models.CharField(max_length=15, blank=False, null=True)
-    country = CountryField(blank_label='Country',blank=True, null=True)
+    user_house_name = models.CharField(max_length=50, blank=False, null=True)
+    user_street_address_1 = models.CharField(max_length=80, blank=False, null=True)
+    user_street_address_2 = models.CharField(max_length=80, blank=True, null=True)
+    user_city = models.CharField(max_length=80, blank=False)
+    user_post_code = models.CharField(max_length=15, blank=False, null=True)
+    user_country = CountryField(blank_label='Country',blank=True, null=True)
     
     def __str__(self):
-        return self.first_name
+        return self.user.username
     
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Update or Create customer profiles
+    """
+    if created:
+        CustomerProfile.objects.create(user=instance)
+    # Existing customers, save profile.
+    instance.userprofile.save()
