735a8e1eef28e3a22b823119383b4d9c2be21462
roomacarthur <roomacarthur@gmail.com>
2022-08-01 05:24:26 +0100

    removed under10 url

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.AllProducts.as_view(), name='products'),
     path('<int:pk>', views.ProductDetails.as_view(), name='product_detail'),
-    path('under-10/', views.UnderTen.as_view(), name='under-10'),
     path('add_product', views.AddNewProduct.as_view(), name='add_product'),
     path('<int:pk>/edit/', views.EditProduct.as_view(), name='edit_product'),
     path('<int:pk>/delete/', views.DeleteProduct.as_view(), name='delete_product'),
