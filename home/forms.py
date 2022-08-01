220da08794e703b56ba2518979b30e083bad6f42
roomacarthur <roomacarthur@gmail.com>
2022-08-01 05:35:59 +0100

    tidyup docstrings.

--- home/forms.py
+++ home/forms.py
@@ -1,13 +1,21 @@
 from .models import ContactUs, NewsLetter
 from django.forms import ModelForm
 
+
 class ContactForm(ModelForm):
+    """
+    Render form with all fields from the ContactUs model
+    """
     class Meta:
         model = ContactUs
         fields = '__all__'
-        
+
 
 class NewsLetterForm(ModelForm):
+    """
+    Render form with only the email field from
+    NewsLetter mode.
+    """
     class Meta:
         model = NewsLetter
         fields = (
