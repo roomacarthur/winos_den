5c1ad25ccc60d2aa29c5edd4da5d3f4b66c8f26a
roomacarthur <roomacarthur@gmail.com>
2025-01-16 14:16:59 +0000

    Adds: url paths for basic new pages

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
     path('contact_us/', views.ContactUs.as_view(), name='contact_us'),
     path('subscribe/', views.NewsLetter.as_view(), name='subscribe'),
+    path('shipping/', views.ShippingView.as_view(), name='shipping'),
+    path('faq/', views.FaqView.as_view(), name='faq'),
+    path('policies/', views.PolicyView.as_view(), name='policies'),
 ]
